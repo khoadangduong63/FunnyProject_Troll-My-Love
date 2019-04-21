599c690cd7f129f3a0d2ff9c47aafa3a3d65e9e6
= <=>
2019-04-21 19:36:16 +0700

    Final commit

---
+++ I love you/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace I_love_you
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCo = new System.Windows.Forms.Button();
+            this.btnKhong = new System.Windows.Forms.Button();
+            this.btnTroll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label4.Font = new System.Drawing.Font("VNI-Ariston", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(114, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(502, 66);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Em coù yeâu anh khoâng?";
+            // 
+            // btnCo
+            // 
+            this.btnCo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnCo.Font = new System.Drawing.Font("VNI-Ariston", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCo.Location = new System.Drawing.Point(44, 152);
+            this.btnCo.Name = "btnCo";
+            this.btnCo.Size = new System.Drawing.Size(207, 53);
+            this.btnCo.TabIndex = 4;
+            this.btnCo.Text = "Daï coù ahihi";
+            this.btnCo.UseVisualStyleBackColor = false;
+            this.btnCo.Click += new System.EventHandler(this.btnCo_Click);
+            // 
+            // btnKhong
+            // 
+            this.btnKhong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnKhong.Font = new System.Drawing.Font("VNI-Ariston", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKhong.Location = new System.Drawing.Point(496, 152);
+            this.btnKhong.Name = "btnKhong";
+            this.btnKhong.Size = new System.Drawing.Size(207, 53);
+            this.btnKhong.TabIndex = 5;
+            this.btnKhong.Text = "EÙo nheù!";
+            this.btnKhong.UseVisualStyleBackColor = false;
+            this.btnKhong.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnKhong_MouseMove);
+            // 
+            // btnTroll
+            // 
+            this.btnTroll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnTroll.Font = new System.Drawing.Font("VNI-Ariston", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTroll.Location = new System.Drawing.Point(197, 309);
+            this.btnTroll.Name = "btnTroll";
+            this.btnTroll.Size = new System.Drawing.Size(323, 56);
+            this.btnTroll.TabIndex = 6;
+            this.btnTroll.Text = "Yeâu anh nhieàu laém";
+            this.btnTroll.UseVisualStyleBackColor = false;
+            this.btnTroll.Visible = false;
+            this.btnTroll.Click += new System.EventHandler(this.btnTroll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::I_love_you.Properties.Resources.text_love5a58662677ff8_b96f11f3f3c95e6736e1d96dcf452a56;
+            this.ClientSize = new System.Drawing.Size(742, 442);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnTroll);
+            this.Controls.Add(this.btnKhong);
+            this.Controls.Add(this.btnCo);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "I LOVE YOU - KHOA DANG DUONG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCo;
+        private System.Windows.Forms.Button btnKhong;
+        private System.Windows.Forms.Button btnTroll;
+    }
+}
+
---
+++ I love you/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace I_love_you
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int demKhong = 1;
+        int demCo = 0;
+        bool checkTroll = false;
+        bool checkKhangCu = false;
+
+        private void btnCo_Click(object sender, EventArgs e)
+        {
+            if(demKhong == 1)
+            {
+                MessageBox.Show("Anh biết em sẽ yêu anh từ lần đầu tiên mà \n\nAnh cũng yêu em nhiều lắm ngốc à! Moazzzzz moazzzz!");
+                this.Close(); //Đóng form
+
+            }
+            if (demKhong > 1 && demCo < 4)
+            {
+                demCo++;
+                switch (demCo)
+                {
+                    case 1:
+                        MessageBox.Show("Sao nãy cự tuyệt Không yêu mà ple ple");
+                        break;
+                    case 2:
+                        MessageBox.Show("Hồi nãy phũ anh thế nào, sao giờ bấm Có lia lịa vậy :p \n\nEm không thoát khỏi chương trình này đâu haha");
+                        MessageBox.Show("\n\nBiết lỗi chưa bấm Có lại đi");
+                        break;
+                    case 3:
+                        //if()
+                        MessageBox.Show("Muốn thoát khỏi chương trình bấm vào nút \"Yêu anh nhiều lắm\" bên dưới");
+                        btnTroll.Visible = true;
+                        break;
+                    case 4:
+                        if (checkTroll == true)
+                        {
+                            MessageBox.Show("Ngoan thế anh mới cưng chứ! \n\nAnh cũng yêu em nhiều lắm ngốc à! Moazzzzz moazzzz!");
+                            this.Close();
+                        }
+                        break;
+                }
+            }
+            if(demCo == 4)
+            {
+                if (checkTroll == true)
+                {
+                    MessageBox.Show("Ngoan thế anh mới cưng chứ! \n\nAnh cũng yêu em nhiều lắm ngốc à! Moazzzzz moazzzz!");
+                    this.Close(); //Đóng form
+                }
+                if(checkTroll == false)
+                {
+                    MessageBox.Show("Em lại bỏ bước rùi! \n\nPhải Yêu anh nhiều lắm cơ, anh mới tha cho");
+                    checkKhangCu = true;
+                    demCo = 2;
+                }
+            }
+        }
+      
+        private void btnKhong_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random rd = new Random();
+            int ChieuCao = rd.Next(32, 500);
+            int ChieuRong = rd.Next(211, 400);
+
+
+            this.btnKhong.Location = new System.Drawing.Point(ChieuCao, ChieuRong);
+            if (demKhong == 1)
+            {
+                MessageBox.Show("Anh biết em dối lòng mà haha");
+                demKhong++;
+            }
+            else if (demKhong == 2)
+            {
+                MessageBox.Show("Chọn Có đi. Dối lòng không tốt đâu ;p");
+                demKhong++;
+            }
+            else if (demKhong == 3)
+            {
+                MessageBox.Show("Dành cả tuổi thanh xuân để bấm Không hả?");
+                demKhong++;
+            }
+            else if (demKhong == 4)
+            {
+                MessageBox.Show("Nghe lời anh chọn có đi mà");
+                demKhong++;
+            }
+            else if (demKhong == 5)
+            {
+                MessageBox.Show("ĐM con này lì vãi");
+                demKhong++;
+            }
+            else if (demKhong == 6)
+            {
+                DialogResult dlr = MessageBox.Show("Không cho yêu thì cho \"ấy\" xíu nhé =]]]]","",MessageBoxButtons.YesNo);
+                if(dlr == DialogResult.Yes)
+                {
+                    MessageBox.Show("Haha hứa rùi nha, tối nay mấy giờ ở đâu nè \n\nNhớ nhắn tin cho anh biết nhé!");
+                    MessageBox.Show("Moazzzz moazzzz chụt chụt");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Không chịu thì thôi ple ple");
+                }
+                demKhong++;
+            }
+            else if (demKhong == 7)
+            {
+                MessageBox.Show("Nói rùi mà. Bấm có đi!");
+                demKhong++;
+            }
+            else if (demKhong == 8)
+            {
+                MessageBox.Show("Ngoan cố hả? \n\nAnh cho em bấm tới mãn kinh luôn ;p");
+                demKhong = 2;
+            }
+        }
+
+        private void btnTroll_Click(object sender, EventArgs e)
+        {
+            if (checkKhangCu == false)
+            {
+                MessageBox.Show("Tin người vcl =]]]]]] \n\nEm yêu anh mà anh đâu có yêu em :p");
+                MessageBox.Show("Em vừa chửi thề???? \n\nThôi tha cho em đó ;p \n\nPhải yêu anh nhất quả đất này nghe chưa. \n\nNgoan! Bấm có lại đi");
+                btnTroll.Visible = false;
+                checkTroll = true;
+            }
+            else
+            {
+                MessageBox.Show("Mệt rồi phải không? \n\nĐừng kháng cự nữa nhé =))))) \n\nNgoan! Bấm có lại đi");
+                btnTroll.Visible = false;
+                demCo = 4;
+                checkTroll = true;
+            }
+        }
+    }
+}
